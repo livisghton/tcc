761d71261e0c8d365d93af207fa52275873e9a73
livisghton <lksa@cin.ufpe.br>
2019-08-27 22:56:36 -0300

    adicao do pacote util e a funcao hashMap

---
+++ src/util/hashmap.py
@@ -0,0 +1,60 @@
+#Codigo extraido do stackoverflow
+#https://stackoverflow.com/questions/8703496/hash-map-in-python
+
+class Node:
+    "Cria uma instancia de Hash"
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
+
+class HashMap:
+    def __init__(self):
+        self.store = [None for _ in range(16)]
+    def get(self, key):
+        index = hash(key) & 15
+        if self.store[index] is None:
+            return None
+        n = self.store[index]
+        while True:
+            if n.key == key:
+                return n.value
+            else:
+                if n.next:
+                    n = n.next
+                else:
+                    return None
+    def put(self, key, value):
+        nd = Node(key, value)
+        index = hash(key) & 15
+        n = self.store[index]
+        if n is None:
+            self.store[index] = nd
+        else:
+            if n.key == key:
+                n.value = value
+            else:
+                while n.next:
+                    if n.key == key:
+                        n.value = value
+                        return
+                    else:
+                        n = n.next
+                n.next = nd
+
+"""
+Exemplo de uso
+hm = HashMap()
+hm.put("C", 1)
+hm.put("D", 1)
+hm.put("E", 1)
+hm.put("F", 1)
+hm.put("G", 1)
+hm.put("A", 1)
+hm.put("B", 1)
+# hm.put("C", hm.put("C")+1)
+k = hm.get("C")
+print(hm.get("C"))
+k = k+1
+hm.put("C", k)
+print(hm.get("C"))"""
