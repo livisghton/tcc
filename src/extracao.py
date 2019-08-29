0033fb20ebca367cc77b09f84a4d2193b6351d9c
livisghton <lksa@cin.ufpe.br>
2019-08-29 02:04:34 -0300

    falta implementar a stft

--- src/extracao.py
+++ src/extracao.py
@@ -1,21 +1,38 @@
 from pydub import AudioSegment
 import glob, os
 import util.hashmap as hashMap
+import numpy as np
+#from scipy import signal
+import scipy.io.wavfile as wav
+import matplotlib.pyplot as plt
+import scipy.signal as stft
+
 
 #https://gist.github.com/gchavez2/53148cdf7490ad62699385791816b1ea
 #https://ieeexplore.ieee.org/document/4564924   (artigo interessante)
 #https://kevinsprojects.wordpress.com/2014/12/13/short-time-fourier-transform-using-python-and-numpy/    (site legal sobre stft)
 #https://musicinformationretrieval.com/stft.html
 
-def musicAccount(src):
+def fileAccount(directory, format):
     """Contabiliza a quantiade de musicas no formato mp3 dentro de um diretorio.
     """
-    os.chdir(src)           #muda o diretorio para src
+    os.chdir(directory)           #muda o diretorio para directory
     number = 0
-    for file in glob.glob("*.mp3"):
+    for file in glob.glob("*"+format):
         number = number + 1
 
     return number
+
+
+def fileNames(directory):
+    """
+    Retorna lista com os nomes dos arquivos no diretorio
+    """
+    os.chdir(directory)           #muda o diretorio para directory
+    names = []
+    names = os.listdir()
+    return names
+
 
 
 def readfile(row):
@@ -38,16 +55,16 @@ def timeToMiliSeconds(start, end):
     return int(startTime), int(endTime)
 
 
-def genereteWav(src, dst, chordsFile):
+def wavGenerete(mp3Files, wavFiles, chordsFiles):
     """
     Faz a segmentacao dos arquivos de audio com base nos arquivos de acordes.\n
-    src: diretorio com os arquivos no formato .mp3;\n
-    dst: destivo onde ira ser salvo os arquivos segmentados no formato .wav;\n
-    chordsFile: Arquivo onde esta os mapeamento dos acordes.
+    \t-mp3Files: diretorio com os arquivos no formato .mp3;\n
+    \t-wavFiles: destivo onde ira ser salvo os arquivos segmentados no formato .wav;\n
+    \t-chordsFiles: Arquivo onde esta os mapeamento dos acordes.
     """
 
     actualDirectory = os.getcwd()        #Salva a posicao do diretorio atual
-    number = musicAccount(src)      #contabiliza a quantidade de arquivos mp3 na pasta src
+    number = fileAccount(mp3Files, ".mp3")      #contabiliza a quantidade de arquivos mp3 na pasta mp3Files
     os.chdir(actualDirectory)       #retorna para o diretorio inicial
 
     hm = hashMap.HashMap()
@@ -57,16 +74,15 @@ def genereteWav(src, dst, chordsFile):
     while(i < number):
         name = str(i)
 
-        sound = AudioSegment.from_mp3(src+name+'.mp3')
-        chords = open(chordsFile + name+ '.lab')
+        sound = AudioSegment.from_mp3(mp3Files+name+'.mp3')
+        #sound.set_channels(1)                                   #TODO:Verifica se funciona, converter para mono 
+        chords = open(chordsFiles + name+ '.lab')
         
         for row in chords:
 
             start, end, chord = readfile(row)
-            
-            key = hm.get(chord)
 
-            if(not key):          #Verifica se ja existe o acorde chord
+            if(not hm.get(chord)):          #Verifica se ja existe o acorde chord
                 hm.put(chord, 1)            #Cria uma nova chave
             else:
                 k = hm.get(chord)
@@ -77,81 +93,76 @@ def genereteWav(src, dst, chordsFile):
 
             #abre a musica e corta um trecho e salva em wav com o nome da nota
             fragment = sound[startTime:endTime]
-            fragment.export(dst + chord[0] +  "_in" + str(hm.get(chord)) +".wav", format="wav")
+            fragment.export(wavFiles + chord[0] +  "_in" + str(hm.get(chord)) +".wav", format="wav")
 
         chords.close
         i = i + 1
 
 
+def spectrumGeneration(wavFiles, windows, lengthWindows, jumpWindows):
+    """
+    Gera o espectro de todos os arquivos no diretorio com a configuracao passada.
+    \t-wavFiles: diretorio com os arquivos no formato .wav;
+    \t-windows: tipo de janela
+    \t-lengthWindows: tamanho da janela em milessegundos
+    \t-jumpWindows: salto da janela em milessegundos
+    """
+    actualDirectory = os.getcwd()        #Salva a posicao do diretorio atual
+    names = fileNames(wavFiles)      #contabiliza a quantidade de arquivos wav na pasta extract_wav
+    os.chdir(actualDirectory)       #retorna para o diretorio inicial
+
+
+    i = 0
+    while(i < len(names)):
+        #name = names[i].split("_")
+        samplerate, samples = wav.read(wavFiles + names[i])
+        if(i == 5):
+           #print(samples)
+            #f, t, Zxx = stft.stft(samples[0], samplerate, windows)
+            f, t, Zxx = stft.stft(samples[0])
+            print(Zxx)
+            #plt.imshow(float(Zxx))
+            #plt.ylabel('Frequency [Hz]')
+            #plt.xlabel('Time [sec]')
+            #plt.show()
+        
+        i = i + 1
+
+"""
+def spectrumAnalyzer(fs, data):
+    frequencies, times, spectrogram = signal.spectrogram(data, fs)
+    plt.pcolormesh(times, frequencies, spectrogram)
+    plt.imshow(spectrogram)
+    plt.ylabel('Frequency [Hz]')
+    plt.xlabel('Time [sec]')
+    #plt.show()
+    plt.savefig('spectrumCar1.png', format='png')"""
+
+
 def main():
     """Funcao para gerar trechos de mp3"""
-    """
-    src = "dataset/mp3/"
-    dst = "dataset/wav/"
 
-    startMin = 0
-    startSec = 50
+    #variaveis referente ao diretorio
+    mp3Files = "dataset/mp3/"
+    chordsFiles = "dataset/chords/"
+    wavFiles = "dataset/extract_wav/"
 
-    endMin = 1
-    endSec = 20
+    #variaveis de entrada
+    windows = np.blackman(256)
+    lengthWindows = 1000        #tamanho da janela em milessegundos
+    jumpWindows = 200         #salto da janela em milessegundos
 
-    # Time to miliseconds
-    startTime = startMin*60*1000+startSec*1000
-    endTime = endMin*60*1000+endSec*1000
 
-    # Opening file and extracting segment
-    song = AudioSegment.from_mp3( src+'0'+'.mp3' )
-    extract = song[startTime:endTime]
+    wavGenerete(mp3Files, wavFiles, chordsFiles)
+
+    spectrumGeneration(wavFiles, windows, lengthWindows, jumpWindows)
     
-    # Saving
-    extract.export( '0'+'_extract.mp3', format="mp3")
-    """
-
-    src = "dataset/mp3/"
-    chordsFile = "dataset/chords/"
-    dst = "dataset/extract_wav/"
-    #mp3 = ".mp3"
-
-    genereteWav(src, dst, chordsFile)
     
     """
     hm = hashMap.HashMap()
     hm.put("C7_5", 1)
     print(hm.get("C7_5"))
     print(hm.get("D"))"""
-    
-    """
-    actualDirectory = os.getcwd()        #Salva o diretorio atual
-
-    number = musicAccount(src)
-
-    os.chdir(actualDirectory)       #retorna para o diretorio inicial
-    
-    
-    i = 0
-    #number = 1
-    while(i < number):
-        name = str(i)
-
-        sound = AudioSegment.from_mp3(src+name+mp3)
-        chords = open(chordsFile + name+ '.lab')
-        
-        j=0
-        for row in chords:
-            start, end, chord = readfile(row)
-            
-            startTime, endTime = timeToMiliSeconds(float(start), float(end))
-
-            #abre a musica e corta um trecho e salva em wav com o nome da nota
-            fragment = sound[startTime:endTime]
-            fragment.export(dst + str(j) + "_" +chord[0] +".wav", format="wav")
-            j=j+1
-            #print(fragment)
-
-
-
-        chords.close
-        i = i + 1"""
 
 
 
